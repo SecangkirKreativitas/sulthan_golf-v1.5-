ba8dda8486c33ca7d50bc95c5d2a8eae6161a569
SecangkirKreativitas <agussupriyanto887@gmail.com>
2022-10-16 17:08:32 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -14,3 +14,42 @@ $('.page-scroll').on('click', function(e){
 
 });
 
+//script untuk button gambar pada tampilan order
+var a;
+function show_hide1(){
+	if(a==1){
+		document.getElementById("text1").style.display="inline";
+		return a=0;
+	}else{
+		document.getElementById("text1").style.display="none";
+		return a=1;
+	}
+
+}
+
+let usersSection = document.querySelector(".text2");
+function show_hide2(){
+	//if(a==1){
+		//document.getElementById("text3").style.display="inline";
+		//return a=0;
+	//}else{
+		//document.getElementById("text3").style.display="none";
+		//return a=1;
+	//}
+
+	usersSection.classList.toggle("hide")
+
+}
+
+let imgSection = document.querySelector(".img");
+function show_hide3(){
+	//if(a==1){
+		//document.getElementById("img").style.display="inline";
+		//return a=0;
+	//}else{
+		//document.getElementById("img").style.display="none";
+		//return a=1;
+	//}
+
+	imgSection.classList.toggle("hide")
+}
